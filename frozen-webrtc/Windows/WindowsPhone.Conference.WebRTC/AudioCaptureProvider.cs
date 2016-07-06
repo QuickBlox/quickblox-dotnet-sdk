2c101c5da0efb1acad13ce4c4a642e691f656a73
Eduardo <kren1@inbox.ru>
2016-07-06 12:10:00 +0300

    Move folders for webrtc frozen

---
+++ frozen-webrtc/Windows/WindowsPhone.Conference.WebRTC/AudioCaptureProvider.cs
@@ -0,0 +1,100 @@
+﻿using FM;
+using FM.IceLink.WebRTC;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WindowsPhone.Conference.WebRTC
+{
+    class AudioCaptureProvider : FM.IceLink.WebRTC.AudioCaptureProvider
+    {
+        private Microphone Microphone;
+        private Resampler Resampler;
+        private DispatcherTimer XnaTimer;
+
+        private byte[] OutputBuffer = new byte[16384];
+
+        public override void Initialize(AudioCaptureInitializeArgs captureArgs)
+        {
+            Microphone = Microphone.Default;
+            Microphone.BufferDuration = TimeSpan.FromMilliseconds(100); // 100 minimum
+            Microphone.BufferReady += Microphone_BufferReady;
+
+            Resampler = new Resampler(DesiredClockRate / 16000);
+
+            RunOnUIThread(() =>
+            {
+                XnaTimer = new DispatcherTimer();
+                XnaTimer.Interval = TimeSpan.FromMilliseconds(50);
+                XnaTimer.Tick += delegate { try { FrameworkDispatcher.Update(); } catch { } };
+                XnaTimer.Start();
+            });
+        }
+
+        public override bool Start()
+        {
+            Microphone.Start();
+            return true;
+        }
+
+        void Microphone_BufferReady(object sender, EventArgs e)
+        {
+            var outputLength = Microphone.GetData(OutputBuffer);
+
+            var frame = new AudioBuffer(OutputBuffer, 0, outputLength);
+            if (!Resampler.Resample(frame, true))
+            {
+                Log.Error("Could not resample XNA audio.");
+            }
+
+            if (!frame.ConvertMonoToStereo())
+            {
+                Log.Error("Could not convert XNA audio to stereo.");
+            }
+
+            RaiseFrame(frame);
+        }
+
+        public override void Stop()
+        {
+            Microphone.Stop();
+        }
+
+        public override void Destroy()
+        {
+            XnaTimer.Stop();
+        }
+
+        public override string[] GetDeviceNames()
+        {
+            return new[] { GetLabel() };
+        }
+
+        public override string GetLabel()
+        {
+            return "Microphone";
+        }
+
+        private static void RunOnUIThread(Action action)
+        {
+            var dispatcher = Deployment.Current.Dispatcher;
+            if (dispatcher.CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                var wait = new AutoResetEvent(false);
+                dispatcher.BeginInvoke(() =>
+                {
+                    action();
+                    wait.Set();
+                });
+                wait.WaitOne();
+            }
+        }
+    }
+}
---
+++ frozen-webrtc/Windows/WindowsPhone.Conference.WebRTC/VideoRenderProvider.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FM.IceLink.WebRTC;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Windows.Phone.Media.Capture;
+using Microsoft.Devices;
+using System.IO;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace WindowsPhone.Conference.WebRTC
+{
+    public class VideoRenderProvider : FM.IceLink.WebRTC.VideoRenderProvider
+    {
+        private Rectangle Rectangle;
+        private VideoBrush VideoBrush;
+
+        public VideoRenderProvider()
+        {
+            RunOnUIThread(() =>
+            {
+                Rectangle = new Rectangle();
+                Rectangle.Stretch = Stretch.Fill;
+                Rectangle.HorizontalAlignment = HorizontalAlignment.Stretch;
+                Rectangle.VerticalAlignment = VerticalAlignment.Stretch;
+            });
+        }
+
+        public override void Initialize(VideoRenderInitializeArgs renderArgs)
+        { }
+
+        public override void Render(VideoBuffer frame)
+        {
+        }
+
+        public void LoadCamera(ICameraCaptureDevice camera)
+        {
+            if (VideoBrush == null && camera != null)
+            {
+                RunOnUIThread(() =>
+                {
+                    VideoBrush = new VideoBrush();
+                    VideoBrush.Stretch = Stretch.Uniform;
+                    VideoBrush.SetSource(camera);
+                    Rectangle.Fill = VideoBrush;
+                });
+            }
+        }
+
+        public void UnloadCamera()
+        {
+            if (VideoBrush != null)
+            {
+                RunOnUIThread(() =>
+                {
+                    Rectangle.Fill = null;
+                    Rectangle = null;
+                    VideoBrush = null;
+                });
+            }
+        }
+
+        public void Rotate(int rotate)
+        {
+            RunOnUIThread(() =>
+            {
+                RotateTransform rotateTransform = new RotateTransform();
+                rotateTransform.Angle = rotate;
+                rotateTransform.CenterX = Rectangle.Width / 2;
+                rotateTransform.CenterY = Rectangle.Height / 2;
+                if (Rectangle.RenderTransform != rotateTransform)
+                {
+                    Rectangle.RenderTransform = rotateTransform;
+                }
+            });
+        }
+
+        public override void Destroy()
+        { }
+
+        public override object GetControl()
+        {
+            return Rectangle;
+        }
+
+        private static void RunOnUIThread(Action action)
+        {
+            var dispatcher = Deployment.Current.Dispatcher;
+            if (dispatcher.CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                var wait = new AutoResetEvent(false);
+                dispatcher.BeginInvoke(() =>
+                {
+                    action();
+                    wait.Set();
+                });
+                wait.WaitOne();
+            }
+        }
+    }
+}
